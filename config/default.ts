8ccbf411291b976120dc52af07c80e619c839416
Vedant Nimbarte <nimbartevedant@gmail.com>
2022-12-15 15:07:36 +0530

    Added server config

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-export default {};
+export default {
+  DB_URI: process.env.DB_CONNECTION_URI,
+  HOST: process.env.HOST,
+  PORT: process.env.PORT,
+  SECRET: process.env.SECRET,
+};
