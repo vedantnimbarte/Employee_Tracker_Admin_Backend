e9058cea420f6416becf6a0ab8fe81e0078f38b5
Vedant Nimbarte <nimbartevedant@gmail.com>
2022-12-16 16:20:24 +0530

    Updated leave schema

---
+++ src/models/leave.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { model, Schema } from "mongoose";
+import UsersModel from "./user.model";
+
+const LeaveSchema = new Schema({
+  start_date: { type: Date, required: true },
+  end_date: { type: Date, required: true },
+  description: { type: String, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: UsersModel,
+  },
+  updated_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: UsersModel,
+  },
+  isPaid: { type: Boolean, default: false },
+  status: { type: String, enum: ["APPROVED", "REJECTED", "IN_PROGRESS"] },
+  type: { type: String, enum: ["FULL", "HALF"], required: true },
+  created_at: { type: Date, default: Date.now, required: true },
+  updated_at: { type: Date, default: Date.now, required: true },
+});
+
+const LeaveModel = model("leaves", LeaveSchema);
+
+export default LeaveModel;
