d53796191d06352002fd2233d6efa9ed3c1b33b3
Vedant Nimbarte <nimbartevedant@gmail.com>
2023-01-13 17:41:55 +0530

    added passport token validation middleware

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Express, Request, Response } from "express";
+import passport from "passport";
+import AuthController from "./controllers/auth.controller";
 import leavesController from "./controllers/leaves.controller";
 import timerController from "./controllers/timer.controller";
 import userController from "./controllers/user.controller";
+import "./common/passport";
 
 export default function Router(app: Express) {
   // HEALTHCHECK ROUTE
@@ -9,18 +12,57 @@ export default function Router(app: Express) {
     res.sendStatus(200)
   );
 
+  // AUTH ROUTES
+  app.post("/login", AuthController.login);
+
   // TIMER ROUTES
-  app.get("/timer", timerController.getAllRecords);
-  app.post("/timer", timerController.createTimeSheetEntry);
-  app.get("/timer/:id", timerController.getAllRecordsByUserId);
+  app.get(
+    "/timer",
+    passport.authenticate("jwt", { session: false }),
+    timerController.getAllRecords
+  );
+  app.post(
+    "/timer",
+    passport.authenticate("jwt", { session: false }),
+    timerController.createTimeSheetEntry
+  );
+  app.get(
+    "/timer/:id",
+    passport.authenticate("jwt", { session: false }),
+    timerController.getAllRecordsByUserId
+  );
 
   // USERS ROUTES
-  app.get("/user", userController.get);
-  app.post("/user", userController.create);
-  app.delete("/user/:id", userController.delete);
-  app.put("/user/:id", userController.update);
+  app.get(
+    "/user",
+    passport.authenticate("jwt", { session: false }),
+    userController.get
+  );
+  app.post(
+    "/user",
+    passport.authenticate("jwt", { session: false }),
+    userController.create
+  );
+  app.delete(
+    "/user/:id",
+    passport.authenticate("jwt", { session: false }),
+    userController.delete
+  );
+  app.put(
+    "/user/:id",
+    passport.authenticate("jwt", { session: false }),
+    userController.update
+  );
 
   // LEAVE ROUTES
-  app.get("/leave", leavesController.list);
-  app.post("/leave", leavesController.create);
+  app.get(
+    "/leave",
+    passport.authenticate("jwt", { session: false }),
+    leavesController.list
+  );
+  app.post(
+    "/leave",
+    passport.authenticate("jwt", { session: false }),
+    leavesController.create
+  );
 }
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,14 @@ import logger from "./common/logger";
 import config from "config";
 import router from "./router";
 import initializeDBConnection from "./common/database";
+import passport from "passport";
+import "./common/passport";
 
 const app: express.Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
 const port = config.get("PORT") as number;
 const host = config.get("HOST") as string;
